d134b6a06e7c4dc13a9ec4f5aa71d563c6d877f1
hxj5 <hxj5@hku.hk>
2024-01-26 13:15:26 +0800

    plp::utils: add get_fine_xclone_phased_snp

--- scsc/plp/utils.py
+++ scsc/plp/utils.py
@@ -1,7 +1,11 @@
 # Utils
 
 import os
+from sys import stdout, stderr
+
+from ..blib.region import Region, RegionSet
 from ..blib.zfile import zopen
+from .region import load_snp_from_tsv, load_snp_from_vcf
 
 
 def _fmt_line(ln, k):
@@ -109,4 +113,63 @@ def rewrite_mtx(in_fn, in_format,
     if remove:
         os.remove(in_fn)
     return(0)
+
+
+class XClonePhasedGene(Region):
+    def __init__(self, chrom, start, end, name, allele_flip):
+        super().__init__(chrom, start, end, name)
+        self.allele_flip = allele_flip
+
+
+def load_xclone_phased_gene(fn, sep = "\t", verbose = False):
+    func = "load_xclone_phased_gene"
+    fp = zopen(fn, "r")
+    nl = 0
+    rs = RegionSet(is_uniq = True)
+    for line in fp:
+        nl += 1
+        if nl == 1:
+            continue
+        items = line.strip().split(sep)
+        if len(items) < 5:
+            raise IOError
+        gene_name, chrom, start, end, allele_flip = items[0], items[2], items[3], items[4], items[-1]
+        start, end = int(start), int(end)
+        if allele_flip == "True":
+            allele_flip = True
+        elif allele_flip == "False":
+            allele_flip = False
+        else:
+            raise ValueError
+        reg = XClonePhasedGene(chrom, start, end + 1, gene_name, allele_flip)
+        rs.add(reg)
+    fp.close()
+    return(rs)
+
+
+def __format_xclone_snp(snp):
+    s = "\t".join([snp.chrom, str(snp.pos), snp.ref, snp.alt, str(snp.ref_idx), str(snp.alt_idx)])
+    return(s)
+
+
+def get_fine_xclone_phased_snp(snp_fn, gene_fn, out_fn, verbose = False):
+    func = "get_fine_xclone_phased_snp"
+    snp_rs = load_snp_from_vcf(snp_fn, verbose = verbose)
+    gene_rs = load_xclone_phased_gene(gene_fn, sep = "\t", verbose = verbose)
+    out_fp = open(out_fn, "w")
+
+    snp_list = snp_rs.get_regions(sort = True)
+    for snp in snp_list:
+        hits = gene_rs.fetch(snp.chrom, snp.start, snp.end)
+        if not hits:
+            s = __format_xclone_snp(snp)
+            out_fp.write(s + "\n")
+        else:     # CHECK ME! what if more than 1 hits?
+            reg = hits[0]
+            if reg.allele_flip:
+                snp.ref_idx = 1 - snp.ref_idx
+                snp.alt_idx = 1 - snp.alt_idx
+            s = __format_xclone_snp(snp)
+            out_fp.write(s + "\n")
+    out_fp.close()
 
